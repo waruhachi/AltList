8ad652526636465d9997ad953f12ed04447dca98
waru <156133757+waruhachi@users.noreply.github.com>
2025-03-06 16:26:38 -0600

    [feat] roothide support

--- install_to_theos.sh
+++ install_to_theos.sh
@@ -8,4 +8,9 @@ make FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
 mkdir -p "$THEOS/lib/iphone/rootless/lib"
 cp -Rv "./.theos/obj/AltList.framework" "$THEOS/lib/iphone/rootless"
 
+make clean
+make FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=roothide
+mkdir -p "$THEOS/lib/iphone/roothide/lib"
+cp -Rv "./.theos/obj/AltList.framework" "$THEOS/lib/iphone/roothide"
+
 echo "Successfully installed AltList"
