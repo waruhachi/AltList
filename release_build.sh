70e0567e28eaa576c4014817b1c8cc1b7a61a739
waru <156133757+waruhachi@users.noreply.github.com>
2025-03-09 16:51:23 -0500

    [feat] update build script to support roothide

--- release_build.sh
+++ release_build.sh
@@ -1,11 +1,14 @@
-#/bin/sh
+#!/bin/sh
 
 export PREFIX=$THEOS/toolchain/Xcode11.xctoolchain/usr/bin/
 
 make clean
 make package FINALPACKAGE=1
 
-export -n PREFIX
-
 make clean
 make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
+
+make clean
+make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=roothide
+
+export -n PREFIX
